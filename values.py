10043494538afc1a01d2dbfca4ccd4ac74150ffc
sofie-rk <sofierk@stud.ntnu.no>
2021-04-26 14:35:33 +0200

    cleaned values

--- values.py
+++ values.py
@@ -1,11 +1,12 @@
-rho_p = 1200    # [kg/m3] 
-rho_c = 6000    # [kg/m3]
+rho_p = 1200            # [kg/m3] 
+rho_c = 6000            # [kg/m3]
 L_F = 0.55 * 10**(-3)   # [m]
 
-M_Cu2O = 0.143  # [kg Cu2O/mol]
-M_unit = 0.574  # [kg unit/mol] 
-M_TBT = 0.2897  # [kg TBT/mol]  
-r_TBT = 4.2 * 10**(-6) * 10**(4) # [kg TBT/m^2 day]
+M_Cu2O = 0.143          # [kg Cu2O/mol]
+M_unit = 0.574          # [kg unit/mol] 
+M_TBT = 0.2897          # [kg TBT/mol]  
+
+r_TBT = 4.2 * 10**(-6)  # [kg TBT/m^2 day]
 
 V_p = 0.7   # volume fraction of polymer
 V_c = 0.3   # volume fraction of pigment
